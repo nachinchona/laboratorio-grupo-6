a47e66a6f5ae5bb28023a55b10b6d8cc73222c3e
nachinchona <ignacio.navarro@est.fi.uncoma.edu.ar>
2024-09-18 15:22:24 -0300

    arreglado contenedor de mas

--- tp-2/js/obtenerDatos.js
+++ tp-2/js/obtenerDatos.js
@@ -21,14 +21,12 @@ Promise.all(urls.map(url => fetch(url)
             imagenPlaylist.src = datos.images[0].url;
             var nombrePlaylist = document.createElement("h3");
             nombrePlaylist.textContent = datos.name;
-            var fotoYNombre = document.createElement("div");
-            fotoYNombre.appendChild(imagenPlaylist);
-            fotoYNombre.appendChild(nombrePlaylist);
             var botonDesplegar = document.createElement("div");
             botonDesplegar.className = "botonDesplegar";
             botonDesplegar.innerHTML = '<label><input type="checkbox"><svg width="30" height="30" viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M12 18L24 30L36 18" stroke="#1E1E1E" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"/></svg></label>';
 
-            playlist.appendChild(fotoYNombre);
+            playlist.appendChild(imagenPlaylist);
+            playlist.appendChild(nombrePlaylist);
             playlist.appendChild(botonDesplegar);
 
             playlists.appendChild(playlist);
