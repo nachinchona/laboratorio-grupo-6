f9b9e786fd4af4776396819bec092b4c1164c62a
MorenoNicolas <nicomore4@gmail.com>
2024-09-18 13:41:54 -0300

    preg

--- tp-2/js/scripts.js
+++
@@ -1,9 +0,0 @@
-
-    const preguntas = document.querySelectorAll(".preguntaEncabezado");
-    
-    preguntas.forEach(pregunta => {
-        pregunta.addEventListener('click',() => {
-            pregunta.nextElementSibling.classList.toggle('activo');
-        })
-    })
-
